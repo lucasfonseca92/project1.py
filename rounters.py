f6ba517962762f5bf45aff58171ad789a3c1361a
Lucas Fonseca <120479634+lucasfonseca92@users.noreply.github.com>
2026-06-27 09:17:24 -0300

    1v_converter

--- rounters.py
+++ rounters.py
@@ -1,10 +1,32 @@
 from fastapi import APIRouter
+from converter import sync_converter
+import time
 
 router = APIRouter()
 
-@router.get("/converter")
-def converter():
-    return "It works"
+# path parameter
+# query parameter
+
+# body parameter
+#/url?to_currencies=USD,EUR,GBP&price=5.55
+
+@router.get("/converter/{from_currency}")
+def converter(from_currency: str, to_currencies: str, price: float):
+    to_currencies = to_currencies.split(",")
+
+    result = []
+
+    for currency in to_currencies:
+        response = sync_converter(
+            from_currency=from_currency,
+            to_currency=currency,
+            price=price
+        )
+
+        result.append(response)
+        time.sleep(1.2)
+
+    return result
 
 
 
